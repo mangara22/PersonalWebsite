d2816b7ee0ce1be92796a6c28684804118ce05dd
Michael Angara <angar016@umn.edu>
2024-05-05 21:22:41 -0500

    cool progress

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const port = 8080;
 
+app.use(express.static('static'));
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
